0b48f9869cad426ab6a9904f5d29693b3603a658
nudinko <simakaze564@gmail.com>
2025-04-24 14:33:27 +0900

    a

--- 01_TCPecho/TCPEcho.h
+++ 01_TCPecho/TCPEcho.h
@@ -1,5 +1,5 @@
 #define MAXPENDING 5   /* 同時にキュー可能な接続要求の最大数 */
-#define RCVBUFSIZE 32  /* 受信バッファのサイズ */
+#define RCVBUFSIZE 4096  /* 受信バッファのサイズ */
 
 void DieWithError(char *errorMessage);  /* エラー処理関数 */
 void HandleTCPClient(int clntSocket);   /* TCPクライアント処理関数 */
