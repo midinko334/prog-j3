bfb5a35830cb8e2e18c29d344131d3de571cccbd
nudinko <simakaze564@gmail.com>
2025-05-28 10:13:11 +0900

    j

--- jikken/No0528_1.c
+++ jikken/No0528_1.c
@@ -1,24 +1,39 @@
-#include <wiringPi.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <pigpiod_if2.h>
+ 
+#define HIGH 1
+#define LOW  0
+#define LEDPIN 17
+#define LOOPCOUNT 10000
+ 
+int main()
+{
+	int pd, t,i;
+ 
+	pd = pigpio_start(NULL,NULL);
+	if (pd < 0){
+		printf("pigpiod の接続に失敗しました。\n");
+		printf("pigpiod が起動しているか確認してください。\n");
+		exit(EXIT_FAILURE);
+	}
+ 
+	printf("明るさ:");
+	scanf("%d",&i);
 
-#define GPIO 17
-
-int main(void){
-
-    int i;
-
-    if(wiringPiSetupGpio()==-1) return 1;
-
-    pinMode(GPIO, PWM_OUTPUT);//GPIOをPWM出力に設定
-    pwmSetMode(PWM_MODE_MS);//markspaceモードに設定
-    pwmSetClock(400);//クロック数を設定
-    pwmSetRange(10);//レンジを設定
-
-    scanf("%d",&i);
-
-    pwmWrite(GPIO,i);
-    delay(5000);
-    pwmWrite(GPIO,0);
-
-    return 0;
+	set_mode(pd,LEDPIN,PI_OUTPUT);
+ 
+	t = 0;
+	while(t < LOOPCOUNT){
+		gpio_write(pd,LEDPIN,HIGH);
+		usleep(i*1000);
+		gpio_write(pd,LEDPIN,LOW);
+		usleep((9-i)*1000);
+		t++;
+	}
+ 
+	pigpio_stop(pd);
+ 
+	return 0;
 }
